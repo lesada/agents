bfa5628b8d06eb120daa881772e35a6f7e876656
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-07-09 16:32:16 -0300

    feat: create room

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,4 @@
 import fastifyCors from "@fastify/cors";
-import fastifySwagger from "@fastify/swagger";
 import fastify from "fastify";
 import {
 	serializerCompiler,
@@ -7,6 +6,7 @@ import {
 	type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { env } from "./env.ts";
+import { createRoomRoute } from "./http/routes/create-room.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -18,21 +18,12 @@ app.register(fastifyCors, {
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(fastifySwagger, {
-	openapi: {
-		info: {
-			title: "Agents Server API",
-			description: "API documentation for the Agents NLW server",
-			version: "1.0.0",
-		},
-	},
-});
-
 app.get("/health", () => {
 	return { message: "Ok" };
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
 
 app.listen({ port: env.PORT }, () => {
 	console.log(`Server listening at http://localhost:${env.PORT} 🚀`);
