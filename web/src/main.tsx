7e2e2860e92110115062ef3b34719ff8312e3195
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-07-09 15:05:40 -0300

    chore: tailwind config

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app.tsx";
+import "./index.css";
 
+// biome-ignore lint/style/noNonNullAssertion: We are sure that the root element exists.
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
